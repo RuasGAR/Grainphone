b92e5075d29a5e163ee5bf0cea1f78a205e9db7e
Gabriel Ruas <gabrielamaralruas@gmail.com>
2024-01-06 06:24:09 +0000

    + workflow quase pronto, tabela atualizada de conversão

--- PyCode/text_to_phone.py
+++ PyCode/text_to_phone.py
@@ -4,12 +4,13 @@ from phonemizer.separator import Separator
 from phonemizer.punctuation import Punctuation
 from phonemizer.backend.espeak.wrapper import EspeakWrapper
 from typing import Dict, List
+from translation_dict import ipa_to_arpabet
 import pandas as pd
 import os
 
 EspeakWrapper.set_library('C:\Program Files\eSpeak NG\libespeak-ng.dll')
 DATASET = pd.read_csv('PyCode\production_data.csv', index_col=0)
-print(DATASET.head(5))
+
 class TextPhone:
     
     PUNCT = Punctuation(';:,.!"?()-')
@@ -18,6 +19,9 @@ class TextPhone:
     def __init__(self,backend_lang:str) -> None:
         self.language = backend_lang
         self.BACKEND = EspeakBackend(backend_lang)
+        self.dataset = DATASET
+        self.available_phones = list(DATASET["phone"].unique())
+        self.phonetic_dict = ipa_to_arpabet
 
     def break_text_in_words(self, text:str):
         text = self.PUNCT.remove(text) 
@@ -27,21 +31,37 @@ class TextPhone:
     def get_lexicon(self, words:List[str]) -> Dict[str, List[str]]:
         
         lexicon = {
-            word: self.BACKEND.phonemize([word], separator=self.SEPARATOR,strip=True, njobs=1)
+            word: self.BACKEND.phonemize([word], separator=self.SEPARATOR,strip=True, njobs=1)[0].split(' ')
             for word in words
         }
         return lexicon            
 
     def rebuild_phrase_with_lexicon(self, words:List[str], lexicon: Dict[str, List[str]]) -> List[List[str]]:
-        
         phrase = []
-        
         for w in words:
-            phrase.append(lexicon[w])        
-        
+            phrase.append(lexicon[w])            
         return phrase
-        
-
+    
+    def check_phonema(self, phone):
+        if phone not in self.available_phones:
+            try:
+                phone = self.phonetic_dict[phone]
+                print(phone)
+                if phone not in self.available_phones:
+                    print("NOT_FOUND: Unavailable phone audio, even after conversion. Default applied.")
+                    print(f"Associated Phone: {phone}")
+                    print("\n")
+                    return "aa"
+                else:
+                    return phone
+            except KeyError as e:
+                print("KEY_ERROR: Unavailable phone audio. Default applied.")
+                print(f"Associated Phone: {e}")
+                print("\n")
+                return "aa"
+        else:
+            return "aa"
+            
     """ 
         For each entry in the dictionary, specifically, for each phone in its repr list,
         it is required to search for the matching audio file of the specific phone.
@@ -50,31 +70,35 @@ class TextPhone:
         every piece from randomly selected people.
     """
 
-    def get_matching_filepaths(self, phrase:List[List[str]]) -> None:
+    def get_matching_filepaths(self, phrase:List[List[str]]) -> List[List[str]]:
         
-        for phone_repr_word in phrase:
-            # I need to access the dataset and recover one path for each of the 
-            # I can make a simple function that goes after the dataset for it;
-            # And then use it to map each phone to its chosen_file;
-            #  
-            # file_paths
-            pass
-            
-            
-
-
-    # this function should be static too, see what are the rules for it
-    def get_audio_filepath(phone, n_speaker) -> str:
-
-        # have to see how it's going to roll off, how I'm building the database, etc;
-        pass
-
-     ##### BEFORE PUSHING, DO THE GIT IGNORE ACCORDING TO THE VENV FILE AND SEE HOW TO SET REQUIREMENTS!!!
+        # query lambda function -> select a random sample from the phone category
+        # and retrieve its wav path
+        search_random_sample = lambda p: self.dataset.query(f'phone == "{p}"').sample(1)["wav_path"].values[0]
+       
+        # has to go over the index, so it doesn't create a new copy
+        for i in range(len(phrase)):
+            try:
+                word_in_phone = phrase[i]
+                word_in_phone = list(map(self.check_phonema,word_in_phone))
+                word_in_phone = list(map(search_random_sample, word_in_phone))
+                phrase[i] = word_in_phone
+            except Exception as e:
+                print(e);
+                return None
+        return phrase
 
 if __name__ == "__main__":
 
     t_2_phone = TextPhone("en-us")
-    #teste = "This is the weirdest thing I have ever seen"
+    test = "This is the weirdest thing I have ever seen"
+    test_words = t_2_phone.break_text_in_words(test)
+    test_lex = t_2_phone.get_lexicon(test_words)
+    test_phrase = t_2_phone.rebuild_phrase_with_lexicon(test_words, test_lex)
+    res = t_2_phone.get_matching_filepaths(test_phrase)
+
+    print(res)
+    
 
 """     result = t_2_phone.break_text_in_words(test_text)
     result = t_2_phone.get_lexicon(result)
